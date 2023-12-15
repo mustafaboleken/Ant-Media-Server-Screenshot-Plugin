d40c78e7022719d94e1b85746f269df30eb673da
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2023-12-15 11:07:14 +0300

    Update RestService.java

--- src/main/java/io/antmedia/rest/RestService.java
+++ src/main/java/io/antmedia/rest/RestService.java
@@ -1,14 +1,20 @@
 package io.antmedia.rest;
 
-import jakarta.servlet.ServletContext;
-import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.Context;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.Response.Status;
+import javax.servlet.ServletContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.google.gson.Gson;
